d94e61284cd0ce12893cf4d9720c8ac22e58bc68
Ramindu Jayasinghe <2021cs086@stu.ucsc.cmb.ac.lk>
2025-05-31 21:14:38 +0530

    complete the transaction handling part

---
+++ src/transactions/transaction.rs
@@ -0,0 +1,14 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Transaction {
+    pub sender: String,
+    pub receiver: String,
+    pub amount: f64,
+}
+
+impl Transaction {
+    pub fn new(sender: String, receiver: String, amount: f64) -> Self {
+        Self { sender, receiver, amount }
+    }
+}
